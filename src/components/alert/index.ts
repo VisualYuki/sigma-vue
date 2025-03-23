6b79976dcbc41ac3165ea526554eea1e51dd290f
denis m. <denis.moshkin.007@gmail.com>
2025-03-23 11:02:53 +0300

    optimization

--- src/components/alert/index.ts
+++ src/components/alert/index.ts
@@ -1,3 +1,5 @@
+import {defineAsyncComponent} from 'vue'
 import Alert from './alert.vue'
 
 export const UiAlert = Alert
+export const UiAsyncAlert = defineAsyncComponent(() => import('./alert.vue'))
--- src/components/checkbox/index.ts
+++ src/components/checkbox/index.ts
@@ -1,3 +1,5 @@
+import {defineAsyncComponent} from 'vue'
 import Checkbox from './checkbox.vue'
 
 export const UiCheckbox = Checkbox
+export const UiAsyncCheckbox = defineAsyncComponent(() => import('./checkbox.vue'))
